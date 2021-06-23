4605908dc00ffbda14f2372e8e9a5648c480e828
Shahrukh-10 <56799965+Shahrukh-10@users.noreply.github.com>
2021-06-23 11:47:28 +0530

    Add files via upload

--- DSA Questions/Array/Union_And_Intersection_Of_Two_Sorted_Arrays.java
+++ DSA Questions/Array/Union_And_Intersection_Of_Two_Sorted_Arrays.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 public class Union_And_Intersection_Of_Two_Sorted_Arrays {
 
     public void intersection(int[] arr, int[] arr1) {
-        int count=0;
+        int count = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr1.length; j++) {
                 if (arr1[j] == arr[i]) {
@@ -14,7 +14,7 @@ public class Union_And_Intersection_Of_Two_Sorted_Arrays {
         System.out.println(count);
     }
 
-    public void union(int[] arr,int[] arr1) {
+    public void union(int[] arr, int[] arr1) {
         HashSet<Integer> hs = new HashSet<>();
         for (int i : arr) {
             hs.add(i);
@@ -23,13 +23,13 @@ public class Union_And_Intersection_Of_Two_Sorted_Arrays {
             hs.add(i);
         }
         System.out.println(hs.size());
-         
+
     }
 
     public static void main(String[] args) {
         Union_And_Intersection_Of_Two_Sorted_Arrays obj = new Union_And_Intersection_Of_Two_Sorted_Arrays();
         int[] arr = { 1, 2, 3, 4, 5, 6 };
-        int[] arr1 = { 4, 8, 9, 7, 5, 6 };
+        int[] arr1 = { 5, 6, 7, 8, 9, 10 };
         obj.intersection(arr, arr1);
         obj.union(arr, arr1);
     }
